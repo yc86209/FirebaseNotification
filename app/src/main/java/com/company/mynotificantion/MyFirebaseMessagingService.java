9e9ce0185a0cc50bcb0dc0c6b50bd168dfe6d7b1
YuChin <yc86209@hotmail.com.tw>
2018-06-07 21:51:21 +0800

    Initial project

---
+++ app/src/main/java/com/company/mynotificantion/MyFirebaseMessagingService.java
@@ -0,0 +1,21 @@
+package com.company.mynotificantion;
+
+import android.util.Log;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
+
+import java.util.Objects;
+import java.util.Set;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+        Log.e("message", Objects.requireNonNull(remoteMessage.getNotification()).getBody());
+        Set<String> keys = remoteMessage.getData().keySet();
+        for(String s:keys) {
+            Log.e("fcm data:", remoteMessage.getData().get(s));
+        }
+    }
+}
